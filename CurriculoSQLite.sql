c9c26f74ef7518fe198b3c258a8e887e4fe11d6c
MrKisi2 <106251998+MrKisi2@users.noreply.github.com>
2022-09-13 09:28:49 -0300

    primeiro commit

---
+++ CurriculoSQLite.sql
@@ -0,0 +1,14 @@
+CREATE TABLE ARIEL_KISILEVZKY(
+ENDERECO VARCHAR(256),
+EMAIL VARCHR(50),
+TELEFONE VARCHAR(20),
+OBJETIVOS TEXT,
+EXPERIENCIA TEXT,
+FORMACAO TEXT
+)
+
+
+INSERT INTO ARIEL_KISILEVZKY(ENDERECO, EMAIL, TELEFONE, OBJETIVOS, EXPERIANCIA, FORMACAO) VALUES ('São Paulo - Rua Guararapes 682 - Lapa - São Paulo', 'arielkisi@hotmail.com', '(11) 9-7558-2348', 'Aprender sobre programação e ser um profissional da área', 'Jogo de educação política feito no Godot para a Ong constituição na escola', 'Estudante: do Colégio Adventista - colegial: Almonte District High School - Inteli Engenharia de Software')
+
+
+INSERT INTO  ARIEL_KISILEVZKY(EXPERIENCIA) VALUES ('No momento, fazendo uma plataforma de gereciamneto de projetos para a Yamaha')
